54ee46a10f3125c737ea096681ecbfa1ae5b0e3d
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2024-08-03 10:16:46 +0700

    :sparkles: tambah component datatable

--- resources/js/components/form/switch-input.tsx
+++ resources/js/components/form/switch-input.tsx
@@ -1,32 +1,16 @@
-import { forwardRef } from "react";
-import { SwitchInputProps } from "@/types/form";
-import { Switch } from "@/components/ui/switch";
+import { forwardRef } from 'react';
+import { SwitchInputProps } from '@/types/form';
+import { Switch } from '@/components/ui/switch';
 
 const SwitchInput = forwardRef<HTMLInputElement, SwitchInputProps>(function SwitchInput(props, ref) {
-   const {
-	  label,
-	  name,
-	  errors,
-	  help,
-	  className,
-	  value,
-	  checked,
-	  ...otherProps
-   } = props
+   const { label, name, errors, help, className, value, checked, ...otherProps } = props;
 
    return (
-	   <div className="grid max-w-sm items-center gap-1.5">
-		  <Switch
-			  name={name}
-			  checked={value}
-			  aria-readonly
-			  {...otherProps}
-		  />
-		  {errors && (
-			  <div className="text-rose-500 text-xs font-normal">{errors}</div>
-		  )}
-	   </div>
-   )
-})
+      <div className="grid max-w-sm items-center gap-1.5">
+         <Switch name={name} id={name} checked={value} aria-readonly {...otherProps} />
+         {errors && <div className="text-rose-500 text-xs font-normal">{errors}</div>}
+      </div>
+   );
+});
 
 export default SwitchInput;
