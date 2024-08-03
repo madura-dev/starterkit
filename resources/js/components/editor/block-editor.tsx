6621cdea58de2fc2aa7c274d3a296c4271f100c6
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2024-08-03 15:18:26 +0700

    :sparkles: component datatable

--- resources/js/components/editor/block-editor.tsx
+++ resources/js/components/editor/block-editor.tsx
@@ -1,18 +1,22 @@
-// src/Tiptap.tsx
 import {BubbleMenu, EditorContent, FloatingMenu, useEditor} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import {
-	DropdownMenu,
-	DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
-	DropdownMenuLabel, DropdownMenuPortal,
-	DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger,
-	DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import {Button} from "@/components/ui/button";
-import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import Placeholder from '@tiptap/extension-placeholder'
 
 // define your extension array
-const extensions = [StarterKit]
+const extensions = [
+	StarterKit,
+	Placeholder.configure({
+		// Use a placeholder:
+		placeholder: 'Write something …', // Use different placeholders depending on the node type:
+		// placeholder: ({ node }) => {
+		//   if (node.type.name === 'heading') {
+		//     return 'What’s the title?'
+		//   }
+
+		//   return 'Can you add some further context?'
+		// },
+	}),
+]
 
 const content = '<p>Hello World!</p>'
 
@@ -30,21 +34,16 @@ const BlockEditor = () => {
 				autocapitalize: 'off',
 			},
 		},
+
 	})
 
-	return (
-		<div className={'flex h-full justify-center '}>
-			<EditorContent editor={editor} className={'flex-1 max-w-[50%] m-10 overflow-y-auto'} />
-			<FloatingMenu editor={editor}>
-				<span className={'text-slate-500 font-light'}>Ketik <kbd>/</kbd> untuk menampilkan opsi</span>
-				<Popover>
-					<PopoverTrigger>Open</PopoverTrigger>
-					<PopoverContent>Place content for the popover here.</PopoverContent>
-				</Popover>
-			</FloatingMenu>
-			<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
-		</div>
-	)
+	return (<div className={'flex h-full justify-center '}>
+		<EditorContent editor={editor} className={'flex-1 max-w-[50%] m-10 overflow-y-auto'} />
+		<FloatingMenu editor={editor}>
+			<span className={'text-slate-500 font-light'}>Ketik <kbd>/</kbd> untuk menampilkan opsi</span>
+		</FloatingMenu>
+		<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+	</div>)
 }
 
 export default BlockEditor
